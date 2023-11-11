fd883e8ee3cbe70183a6f541ee3df5bc362baad2
Techie Wrekie <parasdhiman26@gmail.com>
2023-11-11 19:34:14 +0530

    main entry point name changed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/server/public/'));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 500000000 }));
 app.get('/', (req, res) => {
-    res.send('Welcome to Notes Server')
+    res.send('Welcome to House collab Server')
 })
 
 app.use('/api', apiRoutes)
