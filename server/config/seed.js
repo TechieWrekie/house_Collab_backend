1e9ef4662627547fb25b246896887e2c397eec29
Techie Wrekie <parasdhiman26@gmail.com>
2023-11-12 23:58:00 +0530

    Admin password hashed

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,26 +1,32 @@
-const User = require('../apis/user/userModel')
-
-User.findOne({email:"admin@gmail.com"}).then(userData => {
-    if(userData==null){
+const User = require('../apis/user/userModel');
+const bcrypt = require('bcrypt')
+const salt = bcrypt.genSaltSync(10);
+try {
+  User.findOne({ email: "admin@gmail.com" }).then(userData => {
+    try {
+      if (userData == null) {
         let userData = {
-            userId: 1,
-            name: "Admin",
-            email: "admin@gmail.com",
-            password:"Admin1234#",
-            userType: 1
-        }
-        let user = new User(userData)
+          userId: 1,
+          name: "Admin",
+          email: "admin@gmail.com",
+          password: bcrypt.hashSync("Admin1234#",salt),
+          userType: 1
+        };
+        let user = new User(userData);
         user.save().then(res => {
-            console.log("Admin created")
+          console.log("Admin created");
         }).catch(err => {
-            console.log("Admin create err", err)
-        })
-    }
-    else{
+          console.log("Admin create err", err);
+        });
+      } else {
         console.log("Admin Already Exists");
+      }
+    } catch (err) {
+      console.log("Error in processing user data:", err);
     }
-}).catch(err=>{
-    console.log("Admin create err", err)
-})
-
-
+  }).catch(err => {
+    console.log("Error in finding user:", err);
+  });
+} catch (err) {
+  console.log("Top-level error:", err);
+}
